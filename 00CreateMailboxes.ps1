831015a4c47cd6e2e1533645e41f27fb131671cd
Bill Reed <billreed72@gmail.com>
2013-12-16 21:07:32 -0600

    Update 00CreateMailboxes.ps1
    Cleaned up single quotes and minor formatting.

--- 00CreateMailboxes.ps1
+++ 00CreateMailboxes.ps1
@@ -1,7 +1,7 @@
 $Password=Read-Host "Enter Password" -AsSecureString
 Import-CSV C:\userList.csv |
 foreach {
-$userprincipalname = $_.Firstname + ì.î +  $_.Lastname + ì@dexlab.netî
-$firstandlastname = $_.Firstname + ì î +  $_.Lastname
+$userprincipalname = $_.Firstname + '.' +  $_.Lastname + '@dexlab.net'
+$firstandlastname = $_.Firstname + ' ' +  $_.Lastname
 new-mailbox -name $firstandlastname -alias $_.alias -FirstName $_.Firstname -LastName $_.Lastname -userPrincipalName  $userprincipalname -database "First Storage Group\Mailbox Database" -OrganizationalUnit Users -Password $Password
 }
